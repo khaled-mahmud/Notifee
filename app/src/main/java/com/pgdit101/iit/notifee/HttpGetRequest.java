92189c49710f9e59933c7b5fbd42e63e0d6dc1a1
Khaled Mahmud <30986980+khaled-mahmud@users.noreply.github.com>
2019-11-27 08:53:31 +0600

    Api encoding

---
+++ app/src/main/java/com/pgdit101/iit/notifee/HttpGetRequest.java
@@ -0,0 +1,10 @@
+package com.pgdit101.iit.notifee;
+
+import android.os.AsyncTask;
+
+public class HttpGetRequest extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
